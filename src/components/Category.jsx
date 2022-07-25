cd8b442afa8b0f4ce98cd49feb30cf1178a27ad3
petekgithub <petekshamitbeyli@gmail.com>
2022-07-25 03:36:33 +0300

    category-component-finished

---
+++ src/components/Category.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import styled from 'styled-components'
+import { FaHamburger, FaPizzaSlice } from "react-icons/fa";
+import { GiNoodles, GiChopsticks } from "react-icons/gi";
+
+
+function Category() {
+  return (
+    <List>
+      <Slink to={"cuisine/Italian"}>
+        <FaPizzaSlice />
+        <h3>Italian</h3>
+      </Slink>
+      <Slink to={"cuisine/American"}>
+        <FaHamburger />
+        <h3>American</h3>
+      </Slink>
+      <Slink to={"cuisine/Thai"}>
+        <GiNoodles />
+        <h3>Thai</h3>
+      </Slink>
+      <Slink to={"cuisine/Japanese"}>
+        <GiChopsticks />
+        <h3>Japanese</h3>
+      </Slink>
+    </List>
+  )
+}
+
+const List = styled.div `
+  display: flex;
+  justify-content: center;
+  margin: 3rem 0rem;
+`;
+
+const Slink = styled.div `
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 3rem;
+  text-decoration: none;
+  background: #FFC4C4;
+  width: 6rem;
+  height: 6rem;
+  cursor: pointer;
+  transform: scale(0.9);
+
+  h3 {
+    color: black;
+    font-size: 1rem;
+  }
+
+  svg {
+    color: black;
+    font-size: 2rem;
+  }
+
+  &.active {
+    background: linear-gradient(to right, #9cb4cc, #747474);
+  }
+
+
+
+
+
+
+`;
+
+
+
+
+
+
+export default Category
---
+++ src/components/Search.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import {BsSearch} from 'react-icons/bs';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+
+function Search() {
+  const [input, setInput] = useState("");
+  const navigate = useNavigate();
+  const submitHandler=(e) => {
+    e.preventDefault();
+    navigate("/searched" + input);
+  } 
+
+  return (
+    <FormStyle onSubmit={submitHandler}>
+      <div>
+        <BsSearch></BsSearch> 
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          value={input}
+        />
+      </div>
+    </FormStyle>
+  )
+}
+
+
+const FormStyle = styled.form`
+  margin: 3rem 20rem;
+
+  div {
+    position: relative;
+    width: 100%;
+  }
+
+  input {
+    border: none;
+    background: linear-gradient(30deg, #737DFE, #FFCAC9);
+    font-size: 1.5rem;
+    color: black;
+    padding: 1rem 3rem;
+    border: none;
+    border-radius: 1rem;
+    outline: none;
+    width: 100%;
+    
+  }
+
+  svg {
+    position: absolute;
+    top: 50%;
+    left: 0%;
+    transform: translate(100%, -50%);
+    color: black;
+  }
+`
+
+
+export default Search
