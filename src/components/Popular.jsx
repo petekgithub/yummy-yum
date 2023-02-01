7999a4e99c2932e3884ab37b248486f942a3f1b0
Petech <petekshamitbeyli@gmail.com>
2023-02-02 01:16:57 +0300

    update

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,78 +1,72 @@
-import React, { useEffect, useState } from 'react';
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
-import '@splidejs/react-splide/css';
-import { Link } from 'react-router-dom'
-
-
-
+import "@splidejs/react-splide/css";
+import { Link } from "react-router-dom";
 
 function Popular() {
   const [popular, setPopular] = useState([]);
 
-
   useEffect(() => {
     getVeggie();
-  },[]);
-
+  }, []);
 
   const getVeggie = async () => {
-    const check = localStorage.getItem('veggie');
-    if(check) {
+    const check = localStorage.getItem("veggie");
+    if (check) {
       setPopular(JSON.parse(check));
-    }else {
-        const response = await fetch (
-          `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`
-          );
-          if (!response.ok) {
-            throw new Error("Error fetching users");
-          } 
-        const result = await response.json();
-       //console.log(result);
-        localStorage.setItem("veggie", JSON.stringify(result.recipes));
-        setPopular(result.recipes);
-        //console.log(result.recipes);
+    } else {
+      const response = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`
+      );
+      if (!response.ok) {
+        throw new Error("Error fetching users");
+      }
+      const result = await response.json();
+      //console.log(result);
+      localStorage.setItem("veggie", JSON.stringify(result.recipes));
+      setPopular(result.recipes);
+      //console.log(result.recipes);
     }
-  }
-
-
+  };
 
   return (
     <div>
-      <Wrapper> 
+      <Wrapper>
         <h2>Today's Top Recipies</h2>
-        <Splide options={{
-          perPage:4,
-          arrows: false,
-          pagination: false,
-          drag: 'free',
-          gap: "9rem"
-        }}>
-        {popular.map((recipe) => {
-          return (
-            <SplideSlide key={recipe.id}>
-              <Card>
-                <Link to={"/recipe/" + recipe.id}>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                  <Gradient />
-                </Link>
-              </Card>
-            </SplideSlide>
-          );
-        })}
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "9rem",
+          }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Card>
+                  <Link to={"/recipe/" + recipe.id}>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient />
+                  </Link>
+                </Card>
+              </SplideSlide>
+            );
+          })}
         </Splide>
       </Wrapper>
     </div>
   );
-};
+}
 
-const Wrapper = styled.div `
+const Wrapper = styled.div`
   margin: 2rem 0rem;
 `;
 
-
-const Card = styled.div `
+const Card = styled.div`
   min-height: 20rem;
   border-radius: 1rem;
   overflow: hidden;
@@ -92,7 +86,7 @@ const Card = styled.div `
     z-index: 10;
     left: 50%;
     bottom: 0%;
-    transform: translate(-50%,0%);
+    transform: translate(-50%, 0%);
     color: green;
     width: 100%;
     text-align: center;
@@ -102,9 +96,7 @@ const Card = styled.div `
     display: flex;
     justify-content: center;
     align-items: center;
-
   }
-
 `;
 
 const Gradient = styled.div`
@@ -112,8 +104,7 @@ const Gradient = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
-  background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5));
+  background: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5));
 `;
-
 
 export default Popular;
